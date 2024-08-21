61b5fca7c007eac685929c036dd715092d11d75c
shahzad67432 <shaa1891640@gmail.com>
2024-08-22 03:10:30 +0500

    fixing 'adding key prop in the div for iteratoor'

--- components/Technologies.tsx
+++ components/Technologies.tsx
@@ -1,8 +1,9 @@
 import { technologies } from "@/data"
 
+let i = 0;
 export const Tech = ()=>{
     return technologies.map((n) => (
-        <div className="m-2 inline-flex">
+        <div key={i++} className="m-2 inline-flex">
             <button className="relative inline-flex h-12 w-[50%] md:w-44 md:mt-10  overflow-hidden rounded-lg p-[1px] focus:outline-none">
                 <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                 {/* remove px-3 py-1, add px-5 gap-2 */}
